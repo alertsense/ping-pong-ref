cb7d65e5e799152f2af6de37b5d0b22499935e53
Kevin Kallberg <kevin.kallberg@alertsense.com>
2015-03-10 13:53:46 -0500

    Added IGameService

---
+++ src/AlertSense.PingPong.Common/Interfaces/IGameService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlertSense.PingPong.Common.Interfaces
+{
+    public interface IGameService
+    {
+
+    }
+}
