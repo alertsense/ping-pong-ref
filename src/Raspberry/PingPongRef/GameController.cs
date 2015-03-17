28283fa0426dd75ce286352a79dd67a2cc20a08f
Aaron Jackson <aaron.jackson@alertsense.com>
2015-03-17 07:26:38 -0600

    Adding button hold detection

--- src/Raspberry/PingPongRef/GameController.cs
+++ src/Raspberry/PingPongRef/GameController.cs
@@ -13,6 +13,9 @@ namespace AlertSense.PingPong.Raspberry
         private ITableConnection _table1;
         private ITableConnection _table2;
 
+        private long _buttonDownOn;
+        private bool _buttonDown;
+
         public void Start()
         {
             _table1 = ConnectionFactory.GetTableConnection("Table1");
@@ -24,17 +27,37 @@ namespace AlertSense.PingPong.Raspberry
 
         void Table_ButtonPressed(object sender, ButtonEventArgs e)
         {
-            if (!e.Enabled) return;
-
             var table = (ITableConnection)sender;
-            _t1LedOn = !_t1LedOn;
-            _table1.Led(_t1LedOn);
-            Console.WriteLine("ButtonPressed");
+            Console.WriteLine("_buttonDownOn: {0}, Now: {1}", _buttonDownOn, DateTime.Now.Ticks);
+            var elapsedSpan = new TimeSpan(DateTime.Now.Ticks - _buttonDownOn);
+            if (!e.Enabled) 
+            {
+                if (_buttonDown && elapsedSpan.Milliseconds < 200)
+                {
+                    // Single button press
+                    _t1LedOn = !_t1LedOn;
+                    _table1.Led(_t1LedOn);
+                } 
+                else
+                {
+                }
+                Console.WriteLine("{0}_Button pressed and released in {1} milliseconds", table.Name, elapsedSpan.Milliseconds);
+                _buttonDown = false;
+                return;
+            }
+
+            if (!_buttonDown)
+            {
+                _buttonDownOn = DateTime.Now.Ticks;
+                _buttonDown = true;
+                Console.WriteLine("{0}_Button Down On: {1}", table.Name, _buttonDownOn);
+            }
         }
 
         void Table_Bounce(object sender, BounceEventArgs e)
         {
-            Console.WriteLine("Bounce");
+            var table = (ITableConnection)sender;
+            Console.WriteLine("{0}_Bounce", table.Name);
         }
 
         public void Dispose()
